535b26670aaa50d13e5831d23d6384dd5720461e
abc <abc@example.com>
2018-09-04 10:45:00 +0530

    fixes

--- src/components/ManagerView.jsx
+++ src/components/ManagerView.jsx
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import moment from 'moment';
 import {hrRef} from '../firebase';
 import {postTodayData} from '../actions';
-import {Helmet} from "react-helmet";
+import MetaTags from 'react-meta-tags';
 
 class ManagerView extends Component {
 
@@ -44,13 +44,13 @@ class ManagerView extends Component {
 
     return(
       <div className="container">
-      <Helmet id="two">
+      <MetaTags>
         <title>Admin-AMS</title>
         <meta property="og:title" content="AMS-Admin" />
         <meta property="og:type" content="utility" />
         <meta property="og:url" content="https://markyourattendance.herokuapp.com/manager" />
         <meta property="og:image" content="https://previews.123rf.com/images/carmendorin/carmendorin1405/carmendorin140500034/28009966-grunge-rubber-stamp-with-text-admin-vector-illustration.jpg" />
-      </Helmet>
+      </MetaTags>
         <h2><u>Today's Data</u></h2>
         <div>{this.renderTodayData()}</div>
       </div>
