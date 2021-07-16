860e620c24a603d8013f07ca00d49afbda7fd318
Shivam Gupta <shivamgupta1996@gmail.com>
2021-07-16 22:25:15 +0530

    fixes

--- src/config/firebase_prod.js
+++ src/config/firebase_prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
+    databaseURL: "https://attendance-marking-syste-4b323.firebaseio.com",
     projectId:  process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
